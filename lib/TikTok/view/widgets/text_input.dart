cf22f33089d72b595e368fe66a6de871ddb0539a
RishabhGupta777 <rishabhKrishna841219@gmail.com>
2025-11-07 12:02:06 +0530

    07/11/2025

--- lib/TikTok/view/widgets/text_input.dart
+++ lib/TikTok/view/widgets/text_input.dart
@@ -1,43 +1,65 @@
 import 'package:tiktok_clone/TikTok/constants.dart';
 import 'package:flutter/material.dart';
 
-
-class TextInputField extends StatelessWidget {
+class TextInputField extends StatefulWidget {
   // final - Jab Aapko Pata ho ki ye ab change nahi hoga - const But Widgets/Methods Ke Liye
   final TextEditingController controller;
-  final  IconData myIcon;
+  final IconData myIcon;
   final String myLabelText;
   final bool toHide;
-  TextInputField({Key? key ,
-  required this.controller,
+  TextInputField({
+    Key? key,
+    required this.controller,
     required this.myIcon,
     required this.myLabelText,
     this.toHide = false,
   }) : super(key: key);
 
   @override
+  State<TextInputField> createState() => _TextInputFieldState();
+}
+
+class _TextInputFieldState extends State<TextInputField> {
+  bool _isObscure = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _isObscure = widget.toHide; // Hide text if it’s a password field
+  }
+
+  @override
   Widget build(BuildContext context) {
     return TextField(
-      obscureText: toHide,
-      controller: controller,
+      obscureText:_isObscure,
+      controller: widget.controller,
       decoration: InputDecoration(
-        icon: Icon(myIcon),
-
-        labelText: myLabelText,
+        icon: Icon(widget.myIcon),
+        suffixIcon: widget.toHide
+            ? IconButton(
+                icon: Icon(
+                  _isObscure ? Icons.visibility_off : Icons.visibility,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _isObscure = !_isObscure;
+                  });
+                },
+              )
+            : null,
+        labelText: widget.myLabelText,
         enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(5),
             borderSide: const BorderSide(
               color: borderColor,
             )),
-        focusedBorder:  OutlineInputBorder(
+        focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(5),
-        borderSide: const BorderSide(
-          color: borderColor,
-        ), ),
-
+          borderSide: const BorderSide(
+            color: borderColor,
+          ),
+        ),
       ),
-
-
     );
   }
 }
